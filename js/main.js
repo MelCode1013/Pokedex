c2868f3f7f88941e45aa043aef71a6c4dbb235dd
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2022-05-17 19:32:12 -0400

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const mainScreen = document.querySelector('.main-screen-left')
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-front-image');
@@ -5,22 +6,54 @@ const pokeBackImage = document.querySelector('.poke-back-image');
 const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
 
-fetch('https://pokeapi.co/api/v2/pokemon/1')
+// constants and variables
+
+const types = [
+'normal', 'fighting', 'flying',
+'poison', 'ground', 'rock',
+'bug', 'ghost', 'steel',
+'fire', 'water', 'grass',
+'electric', 'psychic', 'ice',
+'dragon', 'dark', 'fairy'
+]
+
+//functions
+
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
+const resetScreen = () => {
+    for (const type of types) {
+        mainScreen.classList.remove(type);
+    }
+}
+
+
+fetch('https://pokeapi.co/api/v2/pokemon/9')
     .then(res => res.json())
     .then(data => {
-    console.log(data);
 
-    pokeName.textContent = data['name'];
+    resetScreen();
+
+    const dataTypes = data['types'];
+    const dataFirstType = dataTypes[0];
+    const dataSecondType = dataTypes[1];
+
+    pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
+    if (dataSecondType) {
+    pokeTypeTwo.classList.remove('hide');
+    pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
+    } else {
+        pokeTypeTwo.classList.add('hide');
+        pokeTypeTwo.textContent = '';
+    }
+    mainScreen.classList.add(dataFirstType['type']['name']);
+
+    pokeName.textContent = capitalize(data['name']);
     pokeId.textContent = data['id']
 
     const dataSprites = data['sprites']
-    console.log(dataSprites)
-
-    pokeFrontImage.src = dataSprites['versions']['generation-i']['red-blue']['front_default']
-    pokeBackImage.src = dataSprites['versions']['generation-i']['red-blue']['back_default']
-
-    const dataTypes = data['types'];
-    pokeTypeOne.textContent = dataTypes[0]['type']['name']
-    pokeTypeTwo.textContent = dataTypes[1]['type']['name']
+    pokeFrontImage.src = dataSprites['front_default']
+    pokeBackImage.src = dataSprites['back_default']
+    
     })
 
