b1b9a97d60a76b8f49fd0104546e2576d3cd3068
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2022-05-23 22:01:32 -0400

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 const mainScreen = document.querySelector('.main-screen-left')
+const screenImage = document.querySelector('.screen-image')
+const idDisplay = document.querySelector ('.id-paragraph')
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
 const pokeFrontImage = document.querySelector('.poke-front-image');
 const pokeBackImage = document.querySelector('.poke-back-image');
 const pokeTypeOne = document.querySelector('.poke-type-one');
 const pokeTypeTwo = document.querySelector('.poke-type-two');
+const pokeListItems = document.querySelectorAll('.list-item');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button'); 
 
 // constants and variables
 
-const types = [
+const TYPES = [
 'normal', 'fighting', 'flying',
 'poison', 'ground', 'rock',
 'bug', 'ghost', 'steel',
@@ -17,21 +22,54 @@ const types = [
 'dragon', 'dark', 'fairy'
 ]
 
+let prevUrl = null;
+let nextUrl = null;
+
 //functions
 
 const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
 
 const resetScreen = () => {
-    for (const type of types) {
-        mainScreen.classList.remove(type);
-    }
+    screenImage.classList.remove('hide')
+    idDisplay.classList.remove('hide')
+
+    for (const type of TYPES) {
+    mainScreen.classList.remove(type);
+  }
 }
 
 
-fetch('https://pokeapi.co/api/v2/pokemon/9')
+const fetchPokeList = url => {
+    fetch(url)
     .then(res => res.json())
     .then(data => {
 
+        const {results, previous, next} = data;
+        prevUrl = previous;
+        nextUrl = next;
+        
+        
+        for(let i = 0; i < pokeListItems.length; i++){
+            const pokeListItem = pokeListItems[i];
+            const resultData = results[i];
+
+            if (resultData) {
+                const {name, url} = resultData;
+                const urlArray = url.split('/');
+                const id = urlArray[urlArray.length - 2];
+                pokeListItem.textContent = id + '. ' + capitalize(name);
+            } else {
+                pokeListItem.textContent = '';
+            }
+        }
+    });
+
+}
+
+const fetchPokeData = id => {
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(res => res.json())
+    .then(data => {
     resetScreen();
 
     const dataTypes = data['types'];
@@ -56,4 +94,45 @@ fetch('https://pokeapi.co/api/v2/pokemon/9')
     pokeBackImage.src = dataSprites['back_default']
     
     })
+}
 
+const handleRightButtonClick = () => {
+    if (nextUrl) {
+        fetchPokeList(nextUrl);
+    }
+};
+
+const handleLeftButtonClick = () => {
+    if (prevUrl) {
+        fetchPokeList(prevUrl);
+    }
+};
+
+const selectPokemon = (e) => {
+    if(!e.target) return;
+    
+    const listItem = e.target;
+    if (!listItem.textContent) return;
+
+    const id = listItem.textContent.split('.')[0];
+    fetchPokeData(id);
+}
+
+
+//gets data for left side 
+
+ //data for right side
+
+    
+
+
+    //event listeners
+
+    leftButton.addEventListener('click', handleLeftButtonClick)
+    rightButton.addEventListener('click', handleRightButtonClick);
+    for (const pokeListItem of pokeListItems){
+        pokeListItem.addEventListener('click', selectPokemon);
+    }
+    //initialize app
+
+fetchPokeList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=6')
